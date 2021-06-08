814ffcab38d96608a644ebea752986c6dbb82e62
suryanka <83583792+suryanka@users.noreply.github.com>
2021-06-08 09:26:49 +0530

    Update SpeakGoodbye.js

--- module4-solution/SpeakGoodbye.js
+++ module4-solution/SpeakGoodbye.js
@@ -22,10 +22,9 @@
 // xxxx.xxxx = byeSpeaker;
 (function(window) {
 	var speakWord = "Good Bye";
-	var byeSpeaker={};
-	byeSpeaker.speak = function (name) {
+	var byeSpeaker = function (name) {
   		console.log(speakWord + " " + name);
-}
+	}
 	
 	window.byeSpeaker = byeSpeaker;
 
